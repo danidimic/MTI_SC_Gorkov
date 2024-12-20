6531adc5e5eac4e1341a62ceadca7f029ef9722f
danidimic <danidm96@gmail.com>
2024-12-20 12:50:51 +0100

    update

--- GreenFunctions_SC.py
+++ GreenFunctions_SC.py
@@ -22,6 +22,16 @@ def GFnormal(z, Z, kx, ky, mu, Delta, w, t = 1., hbar = 1.):
     kp = 1j*np.sqrt(1./t*(e0+w0)); km = 1j*np.sqrt(1./t*(e0-w0))
 
     return 1j*hbar/(4*t*w0) * ( 1./kp*Wm * np.exp(1j*kp*np.abs(z-Z)) - 1./km*Wp * np.exp(1j*km*np.abs(z-Z)))
+    
+    
+
+# function which computes the normal GF in the SC as a matrix
+def GFnormalMat(z, Z, kx, ky, mu, Delta, w, t = 1., hbar = 1.):
+    
+    # compute diagonal elements
+    GSCdiag =  GFnormal(z=z, Z=Z, kx=kx, ky=ky, mu=mu, Delta=Delta, w=w, t=t, hbar=hbar)
+    
+    return np.array([[GSCdiag,0],[0,GSCdiag]])
 
 
 
@@ -36,6 +46,19 @@ def GFanomalous(z, Z, kx, ky, mu, Delta, w, t = 1., hbar = 1.):
     kp = 1j*np.sqrt(1./t*(e0+w0)); km = 1j*np.sqrt(1./t*(e0-w0))
 
     return -hbar*np.conj(Delta)/(4*t*w0) * ( 1./kp*np.exp(1j*kp*np.abs(z-Z)) - 1./km*np.exp(1j*km*np.abs(z-Z)) )    
+
+
+
+
+# function which computes the normal GF in the SC as a matrix
+def GFanomalousMat(z, Z, kx, ky, mu, Delta, w, t = 1., hbar = 1.):
+    
+    # compute diagonal elements
+    FSCdiag =  GFanomalous(z=z, Z=Z, kx=kx, ky=ky, mu=mu, Delta=Delta, w=w, t=t, hbar=hbar)
+    
+    return np.array([[0,-FSCdiag],[FSCdiag,0]])
+
+
 
 
 
