f980ad10a2c944dda4899b62d9cee06323c301cc
danidimic <danidm96@gmail.com>
2025-01-28 10:35:15 +0100

    MC integral for G2 and F2

--- Reading.py
+++ Reading.py
@@ -2,10 +2,13 @@ import numpy as np
 
 
 
-def Read_GreenFunction(filename, startline = 11):
+# read the files with the 2nd order correction to GF
+# startline = offset to start rreading values
+# zIdx = select z (zIdx=0) or z' (zIdx=1) as independent variable
+def Read_GreenFunction(filename, startline = 11, zIdx=0):
 
 	# path in local folder
-	fn = 'MC-Integrals/Results/Uniform-10000/' + filename
+	fn = 'MC-Integrals/Results/Importance-50000/' + filename
 	
 	# list for z and GF
 	zlattice = []; GF = []
@@ -30,14 +33,24 @@ def Read_GreenFunction(filename, startline = 11):
 					gf.append(value)
 				
 				# add z to list
-				zlattice.append(gf[0].real)
-				# remove z and z'
-				gf = np.delete(gf, [0, 1])	
+				zlattice.append(gf[zIdx].real)
+				# remove z or z'
+				gf = np.delete(gf, [0, 1])
 				# reshape into 4x4 array
 				GF.append(np.reshape(gf, (4,4)))
 
+	
+	# convert to np.array
+	zlattice = np.array(zlattice); GF = np.array(GF)
 
-	return np.array(zlattice), np.array(GF)
+	# sorted indices
+	indices = np.argsort(zlattice)
+	# sorted lattice vector
+	zlattice = zlattice[indices]
+	# sorted GF correspondingly
+	GF = GF[indices]
+	
+	return zlattice, GF
 
 
 #z,gf = Read_GreenFunction('G2-zZ.out')
