5ad5dd6a1b4fa6ef5b0ba852a7bf40b798f8cc2c
danidimic <danidm96@gmail.com>
2025-03-14 09:48:30 +0100

    renaming

--- GreenFunctions_MTI.py
+++ GreenFunctions_MTI.py
@@ -26,7 +26,7 @@ lambdaZ = np.array([[1, 0], [0, -1]])
 
 
 #####################################################################
-####################### NORMAL B.C. SOLUTION ########################
+###################### DIRICHLET B.C. SOLUTION ######################
 #####################################################################
 
 
@@ -218,7 +218,7 @@ def GMTI_diagonal(d, z, Z, kx, ky, L, w, C = -0.0068, D1 = 1.3, D2 = 19.6, M = 0
 
 
 #####################################################################
-##################### MODIFIED B.C. SOLUTION ########################
+####################### MIXED B.C. SOLUTION #########################
 #####################################################################
 
 
@@ -378,6 +378,122 @@ def GMTI_modifiedBC(d, z, Z, kx, ky, L, w, C = -0.0068, D1 = 1.3, D2 = 19.6, A1 
             G[irow][icol] = np.sum( [eMz[irow][j]*cj[j] for j in range(8)] )
         
     return G
+
+
+
+
+
+#####################################################################
+###################### NEUMANN B.C. SOLUTION ########################
+#####################################################################
+
+
+# Function defining the matrix A_12 for the columns 1,2
+# (B.C. vanishing x' and y' at the boundaries - Neumann boundary conditions)
+def Amat_Neumann(d, Z, kx, ky, L, w, C = -0.0068, D1 = 1.3, D2 = 19.6, A1 = 2.2, A2 = 4.1, M = 0.28, B1 = 10, B2 = 56.6, hbar = 1.):
+
+    # exponential at z=0
+    eM0 = expMz(0, kx, ky, L, w, C=C, D1=D1, D2=D2, A1=A1, A2=A2, M=M, B1=B1, B2=B2, hbar=hbar)
+    
+    # exponential at z=d
+    eMd = expMz(d, kx, ky, L, w, C=C, D1=D1, D2=D2, A1=A1, A2=A2, M=M, B1=B1, B2=B2, hbar=hbar)
+
+    # exponential at z=Z
+    eMZ = expMz(Z, kx, ky, L, w, C=C, D1=D1, D2=D2, A1=A1, A2=A2, M=M, B1=B1, B2=B2, hbar=hbar)
+
+    # initialize an empty matrix
+    A = np.empty([16, 16], dtype=complex)
+
+    # fill in the matrix A
+    for i in range(4):
+        for j in range(8):
+
+            # boundary conditions z=0
+            # (vanishing of first-derivative for x-like components)
+            if i==0 or i==1:
+                A[i][j] = eM0[i+4][j]
+                A[i][j+8] = 0
+            # (vanishing of first-derivative for y-like components)
+            if i==2 or i==3:
+                A[i][j] = eM0[i+4][j]
+                A[i][j+8] = 0
+                
+            
+            # boundary conditions z=d
+            # (vanishing of first-derivative for x-like components)
+            if i==0 or i==1:
+                A[i+4][j] = 0
+                A[i+4][j+8] = eMd[i+4][j]
+            # (vanishing of function for y-like components)
+            if i==2 or i==3:
+                A[i+4][j] = 0
+                A[i+4][j+8] = eMd[i][j]
+
+
+            # continuity condition z=z'
+            A[i+8][j] = eMZ[i][j]
+            A[i+8][j+8] = -eMZ[i][j]
+
+            # derivative jump at z=z'
+            A[i+12][j] = eMZ[i+4][j]
+            A[i+12][j+8] = -eMZ[i+4][j]
+
+    return A
+
+
+
+# Function for solving the system and finding the particular solution
+def psolution_Neumann(icol, d, Z, kx, ky, L, w, C = -0.0068, D1 = 1.3, D2 = 19.6, A1 = 2.2, A2 = 4.1, M = 0.28, B1 = 10, B2 = 56.6, hbar=1.):
+
+    # initialize an empty vector
+    y = np.zeros([16], dtype=complex)
+        
+    # select matrix A and vector y
+    match icol:
+    
+        # column 1,2
+        case 1 | 2:
+            # non-homogeneity vector 
+            y[11+icol] = hbar/(B1-D1)
+
+        # column 3,4
+        case 3 | 4:
+            # non-homogeneity vector 
+            y[11+icol] = -hbar/(B1+D1)
+            
+    # matrix of equations
+    A = Amat_Neumann(d, Z, kx, ky, L, w, C=C, D1=D1, D2=D2, A1=A1, A2=A2, M=M, B1=B1, B2=B2, hbar=hbar)
+            
+    return np.linalg.solve(A, y)
+
+
+
+# Function evaluating the Green's function (Z=z', d=thickness)
+def GMTI_NeumannBC(d, z, Z, kx, ky, L, w, C = -0.0068, D1 = 1.3, D2 = 19.6, A1 = 2.2, A2 = 4.1, M = 0.28, B1 = 10, B2 = 56.6, hbar = 1.):
+
+    # empty matrix for Green's function
+    G = np.empty([4, 4], dtype='complex')
+
+    # general solution M_ij at z
+    eMz = expMz(z=z, kx=kx, ky=ky, L=L, w=w, C=C, D1=D1, D2=D2, A1=A1, A2=A2, M=M, B1=B1, B2=B2, hbar=hbar)
+
+    # loop over columns (sigma',lambda')
+    for icol in range(4):
+        
+        # particular solutions c_j
+        cj = psolution_Neumann(icol=icol+1, d=d, Z=Z, kx=kx, ky=ky, L=L, w=w, C=C, D1=D1, D2=D2, A1=A1, A2=A2, M=M, B1=B1, B2=B2, hbar=hbar)
+        
+        # select left or right depending on z,z'
+        cj = cj[:8] if z<Z else cj[8:]
+
+        # loop over the 4 sigma,lambda states i=1,2,3,4
+        for irow in range(4):
+            
+            # evaluate Green's function components 
+            G[irow][icol] = np.sum( [eMz[irow][j]*cj[j] for j in range(8)] )
+        
+    return G
+
 
 
 
---
+++ test.py
@@ -0,0 +1,30 @@
+import numpy as np
+from numpy.linalg import eigh
+
+from scipy.linalg import expm
+from scipy.sparse import bmat
+from scipy.linalg import ishermitian
+
+# Hamiltonian parameters
+params=dict(C = -0.0068, D1 = 1.3, D2 = 19.6, A1 = 2.2, A2 = 4.1, M = 0.28, B1 = 10, B2 = 56.6)
+
+# Set of Pauli matrices for spin
+sigma0 = np.array([[1, 0], [0, 1]])
+sigmaX = np.array([[0, 1], [1, 0]])
+sigmaY = np.array([[0, -1j], [1j, 0]])
+sigmaZ = np.array([[1, 0], [0, -1]])
+
+
+# Set of Pauli matrices for pseudo-spin
+lambda0 = np.array([[1, 0], [0, 1]])
+lambdaX = np.array([[0, 1], [1, 0]])
+lambdaY = np.array([[0, -1j], [1j, 0]])
+lambdaZ = np.array([[1, 0], [0, -1]])
+
+
+
+
+
+
+
+
